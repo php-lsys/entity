50a0a0fef0bae03fd8180e3da0004f1052445fd0
shanliu <shan.liu@msn.com>
2019-09-22 01:23:14 +0800

    .

--- tests/classes/TestLSYSEntity/EntityTest71.php
+++ tests/classes/TestLSYSEntity/EntityTest71.php
@@ -5,13 +5,18 @@ use TestLSYSEntityLib\DomeModelTest;
 use LSYS\Entity;
 class EntityTest71 extends TestCase
 {
-    public function testColumn(){
-        $model1=new DomeModelTest();
-        $e=new Entity($model1);
-        $e->name="fff";
-        $e->values(array(
-            "name"=>"temp"
-        ));
-        $e->asArray();
-    }
+     public function testEntityPkArrChange()
+        {
+            $model1=new DomeModelTestPkArr();
+            $e=new Entity($model1);
+            $e->id=rand(300,5000);
+            $e->code=uniqid("code_");
+            $e->name="ddddddddddddddd";
+            $e->enname="ddddddddddddddd";
+            $e->save();
+            $this->assertTrue($e->saved());
+            $e->enname="dddddddddd";
+            $e->save();
+            $this->assertTrue($e->saved());
+        }
 }
