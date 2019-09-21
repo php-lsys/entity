af5464ad2875f40da4907ec04323b34a4bbd8969
shanliu <shanliu@msn.com>
2019-09-21 14:32:40 +0800

    ..

--- ext/src/class_entity.c
+++ ext/src/class_entity.c
@@ -1574,7 +1574,7 @@ ZEND_METHOD(lsentity_entity_class, asArray){
         zval_ptr_dtor(&columns);
         RETURN_ZVAL(&arr,1,1);
     }
-    RETVAL_ZVAL(data,0,0);
+    RETVAL_ZVAL(data,1,0);
 }
 
 ZEND_METHOD(lsentity_entity_class, jsonSerialize){
