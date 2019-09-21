56351d2b00802bebe1056f73a185a52c19398acf
shanliu <shan.liu@msn.com>
2019-09-22 00:46:31 +0800

    .

--- ext/src/class_entity.c
+++ ext/src/class_entity.c
@@ -253,7 +253,7 @@ ZEND_METHOD(lsentity_entity_class, __set){
 
     zval filter,filterval;
     if(get_filter(object,&filter)){
-        zend_call_method_with_2_params(&filter,Z_OBJCE(filter), NULL, "runfilter",&filterval,&zval_column,value);
+        //zend_call_method_with_2_params(&filter,Z_OBJCE(filter), NULL, "runfilter",&filterval,&zval_column,value);
         RETURN_NULL();
         zval_ptr_dtor(&filter);
        zval_ptr_dtor(&valuecopy);
