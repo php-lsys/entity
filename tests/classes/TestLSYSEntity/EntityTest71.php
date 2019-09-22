4bb44a73d4fc2e57a90cc7a5d4e3d1dcd5a5bb50
shanliu <shanliu@msn.com>
2019-09-22 10:32:26 +0800

    ..

--- tests/classes/TestLSYSEntity/EntityTest71.php
+++ tests/classes/TestLSYSEntity/EntityTest71.php
@@ -10,8 +10,8 @@ class EntityTest71 extends TestCase
         {
             $model1=new DomeModelTestPkArr();
             $e=new Entity($model1);
-            $e->id=rand(300,5000);
-            $e->code=uniqid("code_");
+//             $e->id=rand(300,5000);
+//             $e->code=uniqid("code_");
             $e->name="ddddddddddddddd";
             $e->enname="ddddddddddddddd";
             $e->save();
