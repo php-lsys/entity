a311b161f1f3840622c3bb3ee5381a58b7aabd1b
shanliu <shanliu@msn.com>
2019-09-22 14:17:21 +0800

    ..

--- tests/classes/TestLSYSEntity/EntityTest71.php
+++ tests/classes/TestLSYSEntity/EntityTest71.php
@@ -1,23 +1,16 @@
 <?php
 namespace TestLSYSEntity;
 use PHPUnit\Framework\TestCase;
-use TestLSYSEntityLib\DomeModelTest;
 use LSYS\Entity;
 use TestLSYSEntityLib\DomeModelTestPkArr;
 class EntityTest71 extends TestCase
 {
      public function testEntityPkArrChange()
-        {
+     {//travis-ci php 7.1 存在问题
             $model1=new DomeModelTestPkArr();
             $e=new Entity($model1);
-//             $e->id=rand(300,5000);
-//             $e->code=uniqid("code_");
             $e->name="ddddddddddddddd";
             $e->enname="ddddddddddddddd";
             $e->save();
-//             $this->assertTrue($e->saved());
-//             $e->enname="dddddddddd";
-//             $e->save();
-//             $this->assertTrue($e->saved());
         }
 }
