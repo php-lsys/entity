e28a2c2ffd7a07aeca84046f49bb967e2a684df4
shanliu <shanliu@msn.com>
2019-10-08 20:13:53 +0800

    ..

--- tests/classes/TestLSYSEntity/EntityTest.php
+++ tests/classes/TestLSYSEntity/EntityTest.php
@@ -213,7 +213,7 @@ class LSYSEntity extends TestCase
     public function testEntityDeleteRes(){
         $model1=new DomeModelTest();
         $res=$model1->findByWhere("id<1000000000",3);
-        $this->assertTrue($model1->db()->Builder($model1)->delete($res));
+        $this->assertTrue($model1->db()->exec($model1->db()->Builder($model1)->delete($res)));
     }
     public function testEntityCreateEx(){
         $model1=new DomeModelTest();
